a174f2a6237ae9a193866d2dc0517d6d579969ab
BLUEEAGLE271 <roderickboat@gmail.com>
2026-03-13 16:06:00 +0000

    dubins again

--- src/parking/launch/mission.launch.py
+++ src/parking/launch/mission.launch.py
@@ -241,20 +241,20 @@ def generate_launch_description():
         # 2. Wait 3s for Lidar to spin up, then start Odom
         TimerAction(period=2.0, actions=[rf2o_node]),
         
-        TimerAction(period=4.0, actions=[robot_localization_node]),
+        # TimerAction(period=4.0, actions=[robot_localization_node]),
 
-        # 3. Wait 5s for Odom to stabilize, then start SLAM
-        TimerAction(period=6.0, actions=[slam_node]),
+        # # 3. Wait 5s for Odom to stabilize, then start SLAM
+        # TimerAction(period=6.0, actions=[slam_node]),
 
-        # # # 4. Wait 10s for Map to build, then start Nav2
-        TimerAction(period=8.0, actions=[nav2_launch]),
-        TimerAction(period=15.0, actions=[OpaqueFunction(function=pin_processes)]),
+        # # # # 4. Wait 10s for Map to build, then start Nav2
+        # TimerAction(period=8.0, actions=[nav2_launch]),
+        # TimerAction(period=15.0, actions=[OpaqueFunction(function=pin_processes)]),
 
-        # # # 5. Finally, start your logic
-        TimerAction(period=35.0, actions=[
-             mission_control_node,
-             box_estimator_node,
-             #explore_node
-         ])
+        # # # # 5. Finally, start your logic
+        # TimerAction(period=35.0, actions=[
+        #      mission_control_node,
+        #      box_estimator_node,
+        #      #explore_node
+        #  ])
     ])
 
